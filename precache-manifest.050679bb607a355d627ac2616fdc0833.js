1ed14eed3555fadf4d86e803f19a90c1f9f702cb
syahrilr <syahrilramadhan754@gmail.com>
2021-04-22 17:49:11 +0800

    Updates

---
+++ precache-manifest.050679bb607a355d627ac2616fdc0833.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "702ae1c19833edcc22e10345a4d7a6f2",
+    "url": "/info-covid19/index.html"
+  },
+  {
+    "revision": "db48b5c9c285a1c9baf7",
+    "url": "/info-covid19/static/css/main.4883f533.chunk.css"
+  },
+  {
+    "revision": "7732c4d26e28e1e9cae1",
+    "url": "/info-covid19/static/js/2.e8202881.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/info-covid19/static/js/2.e8202881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db48b5c9c285a1c9baf7",
+    "url": "/info-covid19/static/js/main.9298b2ca.chunk.js"
+  },
+  {
+    "revision": "1fd8856038a0f866184a",
+    "url": "/info-covid19/static/js/runtime-main.f8a960ff.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/info-covid19/static/media/covid-19.d7265326.png"
+  }
+]);
